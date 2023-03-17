85cfad283069d9981008de8b0017dc58a7a421b1
venetak <veneta.kochovska93@gmail.com>
2023-03-17 14:54:24 +0200

    Add comments

--- src/stack.ts
+++ src/stack.ts
@@ -1,24 +1,46 @@
 import { Token } from './token'
 
+/**
+ * @class Stack
+ * Holds the tokenized language phrase in its tokens member.
+ * It can move the tokens one by one in order to make it easier to check
+ * if production is possible.
+ * It can replace item(s) from the tokens with production items.
+ * 
+ * Uses Array.shift and Array.unshift to implement Shift and Reduce in the
+ * context of a shift-reduce parser - https://en.wikipedia.org/wiki/Shift-reduce_parser#Tree_building_steps
+ */
 class Stack {
     tokens: Token[]
     items: Token[]
 
+    /**
+     * Initialize a Stack instance.
+     * @param tokens A tokenized language phrase.
+     */
     constructor (tokens: Token[]) {
         this.tokens = tokens
         this.items = []
     }
 
-    shift (word) {
-        this.tokens.shift()
-        return this.items.unshift(word)
+    /**
+     * Takes the token at the top of the stack and moves it to the items array
+     * so that it could be checked for production.
+     */
+    shift (): void {
+        const tokenAtTop = this.tokens.shift()
+        this.items.unshift(tokenAtTop)
     }
 
-    reduce (start, end, replacement: Token) {
+    /**
+     * Replaces token(s) from the stack items with production items.
+     * @param start The index of the first token to be replaced.
+     * @param end The index of the last token to be replaced.
+     * @param replacement The production item that will replace the tokens.
+     */
+    reduce (start, end, replacement: Token): void {
         this.items.splice(start, end, replacement)
     }
 }
 
-export { 
-    Stack,
-}
+export { Stack }
--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,7 +1,15 @@
+/**
+ * @class Tokenizer
+ * Breaks a language expression to tokens.
+ */
 class Tokenizer {
+    /**
+     * Split a text by space; create an array of tokens.
+     * @param text the text that need to be tokenized.
+     */
     static tokenize (text: string): string[] {
         return text.split(' ')
     }
 }
 
-export default Tokenizer.tokenize
+export { Tokenizer }
